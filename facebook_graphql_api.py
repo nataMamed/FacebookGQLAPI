9856a51685d20e31ddbe4ca7409093820dcdeaee
Natã Carneiro <natacarneiro1@gmail.com>
2022-12-21 10:14:43 -0300

    feature: facebook insights api

---
+++ facebook_graphql_api.py
@@ -0,0 +1,40 @@
+
+import requests
+
+
+
+class FacebookGraphAPI:
+    def __init__(self, account_id, token):
+        self.base_url = f"https://graph.facebook.com/v15.0/act_{account_id}"
+        self.token = token
+
+    def fetch_insights_data(self, filters=''):
+        
+        url = f"{self.base_url}/insights?{filters}"
+
+        headers = {
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        all_data = []
+        counter = 1
+        while True:
+            print("Fetching data, page -> ",counter)
+            response = requests.request("GET", url, headers=headers)
+            all_data.extend(response.json()['data'])
+            try:
+                next = response.json()['paging']['next']
+            except:
+                print("There isn't a next page")
+                break
+            url = next
+            counter += 1
+
+
+if __name__=="__main__":
+    api = FacebookGraphAPI(
+        account_id='',
+        token=''
+    )
+
+    data = api.fetch_insights_data(filters='')
